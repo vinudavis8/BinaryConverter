efd88e5c102683f3842a0580489b5e165d2a5a1e
Vinu Davis <vinudavis8@gmail.com>
2022-12-18 01:01:09 +0530

    Add files via upload

---
+++ Binary/Binary/Binary.cs
@@ -0,0 +1,537 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data.Common;
+using System.Diagnostics.Metrics;
+using System.Dynamic;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.Arm;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CP
+{
+    // Requirements, Assignment/Implementation Rules:
+    //  You must work with 16 bits, hint: Use an int array of 16 as a field
+    //  Binary operators: ~, <<, >>
+    //  Binary, Arithmatic, and Logical operators: +, -, *, /, ==, !=, <, >, <=, >= algorithms
+    //      must perform in binary. Algorthims that convert to denary, perform in denary, and convert
+    //      back to binary carry 0 marks.
+
+    class Binary
+    {
+
+        #region(Fields)
+        private int[] bits;
+        private int[] remainderBits;
+        #endregion
+        #region(Properties)
+        private static int ArraySize = 16;
+        #endregion
+        #region(Constructor)
+        public Binary(int[] value)
+        {
+            bits = value;
+        }
+        public Binary()
+        {
+            bits = new int[ArraySize];
+        }
+
+        #endregion
+        #region(Index operator)
+        public int this[int index]
+        {
+            get
+            {
+                int value = bits[index];
+                return value;
+            }
+        }
+
+        #endregion
+        #region(Implicit Convertors: int to Binary, Binary to int)
+
+        public static implicit operator Binary(int value)
+        {
+
+            try
+            {
+                //steps:
+                //divide integer number by 2 repeatedly
+                //find remainder of each division
+                //reverse order of remainders will be the binary equivalent
+                // if integer number  is negative,find 2s compliment of binary number to get actual binary number
+
+                int[] elements = new int[ArraySize];
+                int rem;
+                int index = 15;
+                bool isNegativeNumber = false;
+                if (value < 0)
+                {
+                    isNegativeNumber = true;
+                    value *= -1;
+                }
+                int temp = value;
+                while (temp > 0)
+                {
+                    temp = value / 2;
+                    rem = value % 2;
+                    elements[index] = rem;
+                    value = temp;
+                    index--;
+                }
+                //since by default int array already has 0 while initialising, we are not adding 0 in rest of the places.
+                if (isNegativeNumber)
+                    return -(new Binary(elements));            //adjusting sign using 2's compliment
+                else
+                    return new Binary(elements);
+            }
+            catch (Exception ex)
+            {
+                return new Binary();
+            }
+
+        }
+
+        public static implicit operator int(Binary value)
+        {
+
+            try
+            {
+                bool isNegative = false;
+                if (value[0] == 1)          //check if binary number is negative
+                {
+                    isNegative = true;
+                    value = -value;         //find 2s compliment of negative number and add sign later
+                }
+                int result = 0;
+                int pow = 0;
+                for (int i = 15; i >= 0; i--)
+                {
+                    result += value[i] * Power(2, pow);
+                    pow++;
+                }
+                if (isNegative)             //adjust sign if number was negative
+                    result = result * -1;
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+        }
+        #endregion
+        #region(Methods: ToDecimal, ToString)
+
+        public decimal ToDecimal()
+        {
+            try
+            {
+                return (int)new Binary(bits);
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+        }
+
+        public override string ToString()
+        {
+            try
+            {
+                string binaryString = "";
+                for (int i = 0; i < bits.Length; i++)
+                {
+                    if (i % 4 == 0)
+                    {
+                        binaryString = binaryString + " " + bits[i];
+                    }
+                    else
+                        binaryString = binaryString + bits[i];
+                }
+                return binaryString;
+            }
+            catch (Exception ex)
+            {
+                return string.Empty;
+            }
+        }
+
+
+        #endregion
+        #region(Shift Opertors: Shift to left by n (<<), Shift to right by n (>>))
+
+
+        public static Binary operator <<(Binary x, int shiftBy)
+        {
+            try
+            {
+                int[] shiftedBits = new int[ArraySize];
+                int startIndex = (x.bits.Count() - 1) - shiftBy;
+                for (int i = startIndex; i >= 0; i--)
+                {
+                    shiftedBits[i] = x[i + shiftBy];
+                }
+                return new Binary(shiftedBits);
+
+            }
+            catch (Exception ex)
+            {
+                return new Binary();
+            }
+
+        }
+        public static Binary operator >>(Binary x, int shiftBy)
+        {
+            try
+            {
+                int[] shiftedBits = new int[16];
+                int startIndex = shiftBy;
+                for (int i = shiftBy; i < x.bits.Count(); i++)
+                {
+                    shiftedBits[i] = x[i - shiftBy];
+                }
+                return new Binary(shiftedBits);
+
+            }
+            catch (Exception ex)
+            {
+                return new Binary();
+            }
+
+        }
+        #endregion
+        #region(Binary Operators: Ones' complement, Negation)
+        public static Binary operator ~(Binary binary)     //ones compliment
+
+        {
+            try
+            {
+                int[] complimentBits = new int[16];
+                for (int i = 0; i < binary.bits.Count(); i++)
+                {
+                    complimentBits[i] = binary[i] == 0 ? 1 : 0;
+                }
+                return new Binary(complimentBits);
+            }
+            catch (Exception ex)
+            {
+                return new Binary();
+            }
+        }
+
+        public static Binary operator -(Binary x)     //2s compliment
+
+        {
+            try
+            {
+                x = ~x;
+                Binary y = 1;
+                Binary compliment = x + y;
+                return compliment;
+            }
+            catch (Exception ex)
+            {
+                return new Binary();
+            }
+        }
+        #endregion
+        #region(Binary Arithmatic Opertors: +, -, *, /)
+
+        public static Binary operator +(Binary x, Binary y)
+        {
+            try
+            {
+                int[] sumBits = new int[16];
+                int carry = 0;
+                int sum = 0;
+                for (int i = x.bits.Count() - 1; i >= 0; i--)
+                {
+                    sum = x[i] + y[i] + carry;
+                    int res = 0;
+                    if (sum == 3)
+                    {
+                        carry = 1;
+                        res = 1;
+                    }
+                    else
+                    if (sum == 2)
+                    {
+                        carry = 1;
+                        res = 0;
+                    }
+                    else
+                    {
+                        carry = 0;
+                        res = sum;
+                    }
+                    sumBits[i] = res;
+                }
+                return new Binary(sumBits);
+            }
+            catch (Exception ex)
+            {
+                return new Binary();
+            }
+
+        }
+        public static Binary operator -(Binary x, Binary y)
+        {
+            try
+            {
+                //A-B  is same as A + (-B) so we are reusing the logic of + operator
+                Binary complimentY = (-y);
+                Binary difference = x + complimentY;
+                return difference;
+            }
+            catch (Exception ex)
+            {
+                return new Binary();
+            }
+
+        }
+
+        public static Binary operator *(Binary x, Binary y)
+        {
+            try
+            {
+                int[] sumBits = new int[16];
+                int[] resultTemp = new int[16];
+                int shiftBy = 0;
+                for (int b = y.bits.Count() - 1; b >= 0; b--)
+                {
+                    resultTemp = new int[16];
+                    for (int a = x.bits.Count() - 1; a >= 0; a--)
+                    {
+                        resultTemp[a] = (y[b] * x[a]);
+                    }
+                    Binary temp = new Binary(resultTemp);
+                    if (shiftBy != 0)
+                        temp = temp <<= shiftBy;                        //shift 
+
+                    Binary binary = new Binary(sumBits) + temp;           //add
+                    sumBits = binary.bits;
+                    shiftBy++;
+                }
+                return new Binary(sumBits);
+            }
+            catch (Exception ex)
+            {
+                return new Binary();
+            }
+        }
+        public static Binary operator /(Binary x, Binary y)
+        {
+            try
+            {
+                //if number is negative, take 2s compliment and adjust sign at end
+                int signX = 1;
+                int signY = 1;
+                if (x[0] == 1)
+                {
+                    x = -x;
+                    signX = -1;
+                }
+                if (y[0] == 1)
+                {
+                    signY = -1;
+                    y = -y;
+                }
+                int[] result = new int[ArraySize];
+                int[] quotientBits = new int[ArraySize];
+                result[ArraySize - 1] = x[0];
+                int[] b = new int[ArraySize];
+                Binary temp = new Binary(result);
+                for (int i = 0; i < x.bits.Count(); i++)
+                {
+                    if (y < temp || y == temp)
+                    {
+                        quotientBits[i] = 1;
+                        b = y.bits;
+                    }
+                    else
+                    {
+                        quotientBits[i] = 0;
+                        b = new int[ArraySize];
+                    }
+                    temp = temp - new Binary(b);
+                    if (i == ArraySize - 1)                      //skip the shifting in first iteration
+                        break;
+                    temp = temp << 1;                          // left shift bits by 1
+                    temp.bits[ArraySize - 1] = x[i + 1];         // add the new bit from top
+
+                }
+                Binary binary = new Binary(quotientBits);      //adjusting sign using 2s compliment if it was negative number
+                if ((signX * signY) == -1)
+                {
+                    binary = -binary;
+                }
+                Binary rem = new Binary(temp.bits);
+                //calculating sign of remainder
+                //If the left operand is negative, then make the result negative.
+                //If the left operand is positive, then make the result positive.
+
+                if (signX == -1)
+                    rem = -rem;
+
+                binary.remainderBits = rem.bits;                    //can be used for modulus operator
+                return binary;
+            }
+            catch (Exception ex)
+            {
+                return new Binary();
+            }
+        }
+
+
+        public static Binary operator %(Binary x, Binary y)
+        {
+            try
+            {
+                Binary result = x / y;
+                return new Binary(result.remainderBits);            //reusing the logic of binary division
+            }
+            catch (Exception ex)
+            {
+                return new Binary(); ;
+            }
+        }
+        #endregion
+        #region(Logical Operators: ==, !=, <, >, <=, >=)
+        public static bool operator ==(Binary x, Binary y)
+        {
+
+            bool flag = true;
+            try
+            {
+                for (int i = 0; i < x.bits.Count(); i++)
+                {
+                    if (x[i] != y[i])
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+                return flag;
+
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        public static bool operator !=(Binary x, Binary y)
+        {
+            try
+            {
+                bool result = (x == y);                     //reusing the lofic of == operator
+                return !result;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        public static bool operator <(Binary x, Binary y)
+        {
+
+            try
+            {
+                //If x < y, then y-x  will be positive, 
+                //If x > y, then y-x  will be negative,
+
+                bool flag = false;
+                if (x == y)
+                    return false;
+                else
+                {
+                    Binary result = y - x;                  //reusing the logic of - operator
+                    if (result[0] == 0)                     // checking if binary  number is positive
+                        flag = true;
+                    else
+                        flag = false;
+                }
+                return flag;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        public static bool operator >(Binary x, Binary y)
+        {
+            try
+            {
+                if (x == y)
+                    return false;
+                else
+                    return !(x < y);                    //reusing the logic of < operator
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public static bool operator <=(Binary x, Binary y)
+        {
+            try
+            {
+                bool flag = false;                       //reusing the logic of == and < operator
+                if (x == y || x < y)
+                    flag = true;
+                return flag;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        public static bool operator >=(Binary x, Binary y)
+        {
+            try
+            {
+                bool flag = false;
+                if (x == y || x > y)                    //reusing the logic of == and > operator
+                    flag = true;
+                return flag;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+
+
+        #endregion
+        #region custom functions
+        private static int Power(int number, int pow)           //methord to calculate power
+        {
+            try
+            {
+                if (pow == 0)
+                    return 1;
+                int result = 1;
+                while (pow != 0)
+                {
+                    result *= number;
+                    pow--;
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+
+        #endregion
+
+    }
+}
---
+++ Binary/Binary/Test.cs
@@ -0,0 +1,171 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace CP
+{
+    internal static class Test
+    {
+        public static void ShiftOperatorTest()
+        {
+            Console.WriteLine("------------------Shift Operator Test------------------");
+
+            Console.WriteLine("assigning 1010 to x");
+            Binary x = 0b1010;   // Notation for literals:  0x-Hex   0b-Binary
+            
+            Console.WriteLine("printing x (0000 0000 0000 1010): {0}", x);
+
+            Console.WriteLine("shifting x by 4 places to the left"); x <<= 4;
+            Console.WriteLine("printing x (0000 0000 1010 0000): {0}", x);
+
+            Console.WriteLine("shifting x 4 places to the right"); x >>= 4;
+            Console.WriteLine("printing x (0000 0000 0000 1010): {0}", x);
+
+            Console.WriteLine("shifting x 12 places to the left"); x <<= 12;
+            Console.WriteLine("printing x (1010 0000 0000 0000): {0}", x);
+
+            Console.WriteLine("shifting x 4 places to the left"); x <<= 4;
+            Console.WriteLine("printing x (0000 0000 0000 0000): {0}", x);
+
+            Console.WriteLine();
+        }
+
+        public static void UnayOperatorTest()
+        {
+            Console.WriteLine("------------------Unary Operator Test------------------");
+
+            Console.WriteLine("assigning 1010 to x");
+            Binary x = 0b1010;
+
+            Console.WriteLine("printing x (0000 0000 0000 1010): {0}", x);
+            Console.WriteLine("printing x in decimal (10): {0}", x.ToDecimal());
+
+            Console.WriteLine("1's complement: printing ~x (1111 1111 1111 0101): {0}", ~x);
+            Console.WriteLine("printing ~x in decimal (-11): {0}", (~x).ToDecimal());
+
+            Console.WriteLine("2's complement: printing -x (1111 1111 1111 0110): {0}", -x);
+            Console.WriteLine("printing -x in decimal (-10): {0}", (-x).ToDecimal());
+
+            Console.WriteLine();
+        }
+
+        public static void ArithmeticOperatorTest()
+        {
+            Console.WriteLine("----------------Arithmetic Operator Test----------------");
+
+            Console.WriteLine("assigning 10 to x");
+            Binary x = 10;
+            Console.WriteLine("assigning -12 to y");
+            Binary y = -12;
+
+            Console.WriteLine("printing x (0000 0000 0000 1010): {0}", x);
+            Console.WriteLine("printing x in decimal (10): {0}", x.ToDecimal());
+            Console.WriteLine("printing y (1111 1111 1111 0100): {0}", y);
+            Console.WriteLine("printing y in decimal (-12): {0}", y.ToDecimal());
+
+            Console.WriteLine("printing x + y (1111 1111 1111 1110): {0}", x + y);
+            Console.WriteLine("printing x + y in decimal (-2): {0}", (x + y).ToDecimal());
+
+            Console.WriteLine("printing x - y (0000 0000 0001 0110): {0}", x - y);
+            Console.WriteLine("printing x - y in decimal (22): {0}", (x - y).ToDecimal());
+
+            Console.WriteLine("printing x * y (1111 1111 1000 1000): {0}", x * y);
+            Console.WriteLine("printing x * y in decimal (-120): {0}", (x * y).ToDecimal());
+
+            Console.WriteLine("printing -x * y (0000 0000 0111 1000): {0}", -x * y);
+            Console.WriteLine("printing -x * y in decimal (120): {0}", (-x * y).ToDecimal());
+
+            x = 10; y = 3;
+            Console.WriteLine("printing x / y (0000 0000 0000 0011): {0}", x / y);
+            Console.WriteLine("printing x / y in decimal (3): {0}", (x / y).ToDecimal());
+            Console.WriteLine("printing x % y (0000 0000 0000 0001): {0}", x % y);
+            Console.WriteLine("printing x / y in decimal (1): {0}", (x % y).ToDecimal());
+            x = 10; y = -3;
+            Console.WriteLine("printing x / y (1111 1111 1111 1101): {0}", x / y);
+            Console.WriteLine("printing x / y in decimal (-3): {0}", (x / y).ToDecimal());
+            Console.WriteLine("printing x % y (0000 0000 0000 0001): {0}", x % y);
+            Console.WriteLine("printing x / y in decimal (1): {0}", (x % y).ToDecimal());
+
+            x = -10; y = 3;
+            Console.WriteLine("printing x / y (1111 1111 1111 1101): {0}", x / y);
+            Console.WriteLine("printing x / y in decimal (-3): {0}", (x / y).ToDecimal());
+            Console.WriteLine("printing x % y (1111 1111 1111 1111): {0}", x % y);
+            Console.WriteLine("printing x / y in decimal (-1): {0}", (x % y).ToDecimal());
+
+            x = -10; y = -3;
+            Console.WriteLine("printing x / y (0000 0000 0000 0011): {0}", x / y);
+            Console.WriteLine("printing x / y in decimal (3): {0}", (x / y).ToDecimal());
+            Console.WriteLine("printing x % y (1111 1111 1111 1111): {0}", x % y);
+            Console.WriteLine("printing x / y in decimal (-1): {0}", (x % y).ToDecimal());
+
+            x = 135; y = 62;
+            Console.WriteLine("printing x / y (0000 0000 0000 0010): {0}", x / y);
+            Console.WriteLine("printing x / y in decimal (2): {0}", (x / y).ToDecimal());
+            Console.WriteLine("printing x % y (0000 0000 0000 1011): {0}", x % y);
+            Console.WriteLine("printing x / y in decimal (11): {0}", (x % y).ToDecimal());
+            x = 135; y = -62;
+            Console.WriteLine("printing x / y (1111 1111 1111 1110): {0}", x / y);
+            Console.WriteLine("printing x / y in decimal (-2): {0}", (x / y).ToDecimal());
+            Console.WriteLine("printing x % y (0000 0000 0000 1011): {0}", x % y);
+            Console.WriteLine("printing x / y in decimal (11): {0}", (x % y).ToDecimal());
+
+            x = -135; y = 62;
+            Console.WriteLine("printing x / y (1111 1111 1111 1110): {0}", x / y);
+            Console.WriteLine("printing x / y in decimal (-2): {0}", (x / y).ToDecimal());
+            Console.WriteLine("printing x % y (1111 1111 1111 0101): {0}", x % y);
+            Console.WriteLine("printing x / y in decimal (-11): {0}", (x % y).ToDecimal());
+
+            x = -135; y = -62;
+            Console.WriteLine("printing x / y (0000 0000 0000 0010): {0}", x / y);
+            Console.WriteLine("printing x / y in decimal (2): {0}", (x / y).ToDecimal());
+            Console.WriteLine("printing x % y (1111 1111 1111 0101): {0}", x % y);
+            Console.WriteLine("printing x / y in decimal (-11): {0}", (x % y).ToDecimal());
+
+            Console.WriteLine();
+        }
+
+        public static void RelationalOperatorTest()
+        {
+            Console.WriteLine("----------------Relational Operator Test----------------");
+
+            Console.WriteLine("assigning 10 to x");
+            Binary x = 10;
+            Console.WriteLine("assigning -12 to y");
+            Binary y = -12;
+            Console.WriteLine("assigning 2 to z");
+            Binary z = 2;
+
+            Console.WriteLine("printing x (0000 0000 0000 1010): {0}", x);
+            Console.WriteLine("printing x in decimal (10): {0}", x.ToDecimal());
+            Console.WriteLine("printing y (1111 1111 1111 0100): {0}", y);
+            Console.WriteLine("printing y in decimal (-12): {0}", y.ToDecimal());
+            Console.WriteLine("printing z (0000 0000 0000 0010): {0}", z);
+            Console.WriteLine("printing z in decimal (2): {0}", z.ToDecimal());
+
+            Console.WriteLine("printing x == y (False): {0}", x == y);
+            Console.WriteLine("printing x != y (True): {0}", x != y);
+            Console.WriteLine("printing -x - z == y (True): {0}", -x - z == y);
+
+            Console.WriteLine("printing x < y (False): {0}", x < y);
+            Console.WriteLine("printing y < x (True): {0}", y < x);
+            Console.WriteLine("printing x < z (False): {0}", x < z);
+            Console.WriteLine("printing z < x (True): {0}", z < x);
+            Console.WriteLine("printing -x < x + y (True): {0}", -x < x + y);
+
+            Console.WriteLine("printing x > y (True): {0}", x > y);
+            Console.WriteLine("printing y > x (False): {0}", y > x);
+            Console.WriteLine("printing x > z (True): {0}", x > z);
+            Console.WriteLine("printing z > x (False): {0}", z > x);
+            Console.WriteLine("printing -z > x + y (False): {0}", -z > x + y);
+
+            Console.WriteLine("printing x <= y (False): {0}", x <= y);
+            Console.WriteLine("printing x >= y (True): {0}", x >= y);
+            Console.WriteLine("printing -z <= x + y (True): {0}", -z <= x + y);
+            Console.WriteLine("printing -z >= x + y (True): {0}", -z >= x + y);
+
+            Console.WriteLine();
+        }
+    }
+}
